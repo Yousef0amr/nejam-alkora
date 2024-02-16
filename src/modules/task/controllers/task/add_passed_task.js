f4bd8c825180ed0deb19abd7b894f7010ee63767
Youssief Amr Mohamed <youssief192677@fci.bu.edu.eg>
2024-02-16 18:24:33 +0200

    up

--- src/modules/task/controllers/task/add_passed_task.js
+++ src/modules/task/controllers/task/add_passed_task.js
@@ -8,23 +8,20 @@ const addPassedTasks = wrap(
     async (req, res, next) => {
         const player = await Player.findById(req.userId)
         const id = req.body.id
-        const task = await PassedTask.findById(id);
+        const task = await PassedTask.findOne({ task: id });
 
-        console.log(task)
         if (task) {
             task.degree = req.body.degree
-            await task.save();
         } else {
-            const task = new PassedTask(
+            task = new PassedTask(
                 {
                     degree: req.body.degree,
                     task: id,
                     player: player.id
                 }
             )
-            await task.save();
         }
-
+        await task.save();
         return Success(res, 'OK', null)
     }
 )

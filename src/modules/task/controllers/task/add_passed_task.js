489301c2e0aa1071f3fa1cda9895cd7c9a5db7cc
Youssief Amr Mohamed <youssief192677@fci.bu.edu.eg>
2024-02-16 17:48:10 +0200

    up

--- src/modules/task/controllers/task/add_passed_task.js
+++ src/modules/task/controllers/task/add_passed_task.js
@@ -1,13 +1,16 @@
 const wrap = require("express-async-wrapper")
 const { PassedTask } = require('./../../task.model')
 const Player = require('./../../../player/player.model')
-const { Success } = require("../../../../utils/apiResponse")
+const { Success, ApiError } = require("../../../../utils/apiResponse")
 
 
 const addPassedTasks = wrap(
     async (req, res, next) => {
         const player = await Player.findById(req.userId)
-
+        const isPassed = await PassedTask.findById(req.body.id);
+        if (isPassed) {
+            return next(new ApiError('Task already passed', 400))
+        }
         const task = new PassedTask(
             {
                 degree: req.body.degree,
